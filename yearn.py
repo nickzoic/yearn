a58aa9f0a04da5b4e020edf9cad63ad30284a20b
Nick Moore <nick@zoic.org>
2025-07-31 15:02:18 +1000

    more work on this, including towns!

--- yearn.py
+++ yearn.py
@@ -2,6 +2,7 @@ import sqlite3
 import zstd
 import random
 from collections import defaultdict
+import random
 
 # The read format (Ultima IV) and write format (minetest) are so different that
 # the easiest way to convert between them is an intermediate format. We could
@@ -54,21 +55,29 @@ block_map = {
 def get_block_byte(itemstring):
     return block_map.setdefault(itemstring, len(block_map))
 
+# Load the Ultima IV world map into memory.  It's only 256x256 bytes!
+
+with open("dat/WORLD.MAP", "rb") as fh:
+    ultima_world = fh.read(256*256)
+
 # This maps the Ultima IV tile types to little stacks of Minetest
 # blocks.
 
 blocks_for_tile = {
-        0: [ 'default:sand', 'default:water_source', 'default:water_source' ],
-        1: [ 'default:sand', 'default:sand', 'default:water_source' ],
-        2: [ 'default:sand', 'default:sand', 'default:sand_with_kelp' ],
-        3: [ 'default:stone', 'default:dirt', 'default:dirt', 'default:junglegrass' ],
-        4: [ 'default:stone', 'default:dirt', 'default:dirt_with_grass' ],
-        5: [ 'default:stone', 'default:dirt', 'default:dirt_with_rainforest_litter' ],
-        6: [ 'default:stone', 'default:dirt', 'default:dirt', 'default:dirt_with_coniferous_litter' ],
-        7: [ 'default:stone', 'default:stone', 'default:stone', 'default:cobble' ],
-        8: [ 'default:stone', 'default:stome', 'default:stone', 'default:stone', 'default:stone', 'default:snow' ],
-        0x17: [ 'default:sand', 'default:water_source', 'default:water_source', 'air', 'default:stone' ],
-        0x4C: [ 'default:lava' ] * 6
+        0: lambda: [ 'default:sand', 'default:water_source', 'default:water_source' ],
+        1: lambda: [ 'default:sand', 'default:sand', 'default:water_source' ],
+        2: lambda: [ 'default:stone', 'default:sand', 'default:river_water_source' ],
+        3: lambda: [ 'default:stone', 'default:dirt', 'default:dirt_with_grass' ],
+        4: lambda: [ 'default:stone', 'default:dirt', 'default:dirt', 'default:dirt_with_grass' ],
+        5: lambda: [ 'default:stone', 'default:dirt', 'default:dirt', 'default:dirt_with_rainforest_litter', random.choice(('default:junglegrass', 'default:junglegrass', 'default:junglesapling')) ],
+        6: lambda: [ 'default:stone', 'default:dirt', 'default:gravel', 'default:dirt', 'default:dirt' ] + random.choices((['default:dirt'], ['default:dirt_with_coniferous_litter'], ['default:dirt', 'default:pine_sapling'], ['default:dirt', 'default:aspen_sapling']), (9,5,1,1))[0],
+        7: lambda: [ 'default:stone' ] * random.randint(4,5),
+        8: lambda: [ 'default:stone' ] * random.randint(8,12) + [ 'default:snow' ],
+        0x16: lambda: [ 'default:stone' ] * 3 + [ 'default:cobble' ],
+        0x17: lambda: [ 'default:stone', 'default:gravel', 'default:water_source', 'air', 'default:stone' ],
+        0x3E: lambda: [ 'default:brick' ] * 4,
+        0x4C: lambda: [ 'default:lava' ] * random.randint(8,10),
+        0x7F: lambda: [ 'default:brick' ] * 8,
 }
 
 # The idea here is to integrate towns into the map so
@@ -79,31 +88,52 @@ blocks_for_tile = {
 # and the towns will just have to hang over into
 # neighbouring tiles.
 
-# mini world for now
-SCALE = 5
+# XXX maybe even 12 is more like it, towns would then 
+# overlap most of the tiles around them but that's okay.
 
-with open("dat/WORLD.MAP", "rb") as fh:
-    ultima_world = fh.read(256*256)
+SCALE = 5
 
 # the Ultima map is broken up into 8x8 chunks
 # each of which is 32x32 tiles.
 
-for tx in range(0, 256):
-    print(tx)
-    for ty in range(0, 256):
+for ty in range(0, 256):
+    print(ty)
+    for tx in range(0, 256):
         tile = ultima_world[
             (ty // 32) * 8192 +
             (tx // 32) * 1024 +
             (ty % 32) * 32 +
             (tx % 32)
         ]
-        blocks = blocks_for_tile.get(tile)
-        if not blocks: continue
+        block_generator = blocks_for_tile.get(tile)
+        if not block_generator:
+            print("Unknown tile %02x at world (%d, %d)" % (tile, tx, ty))
+            continue
+
         for ox in range(SCALE):
             for oy in range(SCALE):
-                for (oz, block) in enumerate(blocks):
+                for (oz, block) in enumerate(block_generator()):
                     bb = get_block_byte(block)
                     set_block(tx*SCALE+ox, oz, ty*SCALE+oy, bb)
+
+def read_town(map_name, x, y, z=0):
+    with open(f"dat/{map_name}.ULT", "rb") as fh:
+        ultima_town = fh.read(32*32)
+        for tx in range(0,32):
+            for ty in range(0,32):
+                tile = ultima_town[ty*32+tx]
+                block_generator = blocks_for_tile.get(tile)
+                if not block_generator:
+                    print("Unknown tile %02x at town %s (%d, %d)" % (tile, map_name, tx, ty))
+                    continue
+                for (oz, block) in enumerate(block_generator()):
+                    bb = get_block_byte(block)
+                    set_block(int((x+0.5)*SCALE)+tx, z + oz, int((y+0.5)*SCALE)+ty, bb)
+
+
+read_town('LCB_1', 107, 86)
+read_town('LCB_2', 107, 86, 10)
+read_town('BRITAIN', 50, 50)
 
 for k, v in block_map.items():
     print(k,v)
